905a4bff60060225989641f7f46638883510c658
kdoni <chrisvdoni@gmail.com>
2022-09-04 19:37:12 +0300

    more ruby

---
+++ age.rb
@@ -0,0 +1,7 @@
+age = "How old are you bro?"
+puts age
+reply = gets.chomp
+puts "In 10 years you will be: \n #{reply.to_i + 10}"
+puts "In 20 years you will be: \n #{reply.to_i + 20}"
+puts "In 30 years you will be: \n #{reply.to_i + 30}"
+puts "In 40 years you will be: \n #{reply.to_i + 40}" 
