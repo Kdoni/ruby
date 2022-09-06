a660fd9696d58697fa235dba32b14632f35af1eb
kdoni <chrisvdoni@gmail.com>
2022-09-06 16:40:11 +0300

    functions in ruby

---
+++ test3.rb
@@ -0,0 +1,2 @@
+print "Hello world";print "I love cringing"
+puts "Hello world";puts "I love cringing"
---
+++ test4.rb
@@ -0,0 +1,7 @@
+def big (string)
+    if string.length>10
+        return string.upcase
+    end
+end
+
+puts big("katsikarovagia")
