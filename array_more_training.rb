842229269ccd57a66973675dac413a281616ff65
kdoni <chrisvdoni@gmail.com>
2022-09-08 18:47:48 +0300

    More arrays.

---
+++ array_more_training.rb
@@ -0,0 +1,4 @@
+arr = [3,2,3,5,6,7,8]
+new_arr = arr.map {|num| num +2}
+p arr
+p new_arr
--- array_training.rb
+++ array_training.rb
@@ -1,2 +1,5 @@
 arr = ["a","b","d","e","w"]
-arr.each_with_index {|val,index| puts "#{index}.#{val}" }
+# arr.each_with_index {|val,index| puts "#{index}.#{val}" }
+arr.each_with_index do |val,index|
+    puts "#{index}.#{val}"
+end
