413aa2fce31f3e0822cf86e34c079d378fd7c2f7
kdoni <chrisvdoni@gmail.com>
2022-09-19 15:44:55 +0300

    Each_with_index chained with enumerables.

--- practice_map.rb
+++ practice_map.rb
@@ -1,1 +1,5 @@
-p (-100..0).map{|x| x}.reverse
+# p (-100..0).map{|x| x}.reverse
+
+ark = %w[cat dog pig goat]
+result = ark.each_with_index.map {|animal,index| animal.capitalize.reverse if index%2==1}
+puts result
